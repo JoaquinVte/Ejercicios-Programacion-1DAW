aa4d98e05ff1596c4a0422983afab9455a7cc205
Joaquin Vicente Alonso Saiz <joaalsai@ieslavereda.es>
2018-03-25 17:18:19 +0200

    Ejercicio 7

--- src/ejercicios/Ej7.java
+++ src/ejercicios/Ej7.java
@@ -1,5 +1,33 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Ej7 {
 
+	public static void main(String[] args) {
+		int[] array = new int[10];
+		Scanner entrada = new Scanner(System.in);
+
+		int positivos = 0;
+		int negativos = 0;
+		int ceros = 0;
+
+		for (int i = 0; i < array.length; i++) {
+			System.out.print("Dame el valor para la posicion " + i + " del array: ");
+			array[i] = entrada.nextInt();
+		}
+
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == 0) {
+				ceros++;
+			} else {
+				if (array[i] > 0)
+					positivos++;
+				else
+					negativos++;
+			}
+		}
+		System.out.printf("La cantidad de numeros positivos es %s, de negativos %s y de ceros %s.", positivos,negativos,ceros);
+		entrada.close();
+	}
 }
