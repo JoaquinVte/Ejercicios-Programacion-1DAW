29abbdf49b43c1ea85359433b2f297d4133550f7
Joaquin Vicente Alonso Saiz <joaalsai@ieslavereda.es>
2018-03-26 14:15:41 +0200

    Ej10

--- src/ejercicios/Ej10.java
+++ src/ejercicios/Ej10.java
@@ -28,68 +28,129 @@ public class Ej10 {
 
 		rellenarArray(personas);
 
-		System.out.println(mostrarArray(personas));
+		System.out.println("Los datos con en el vector son:");
 
-//		System.out.println("La altura media es " + obtenerAlturaMedia(personas));
-//
-//		mostrarPersonaMasAlta(personas);
-//
-//		mostrarPersonaMasBaja(personas);
-//
-//		listarSuperiorALaMedia(personas);
-//
-//		listarInferiorALaMedia(personas);
+		mostrarArray(personas);
+
+		System.out.printf("La altura media es %.2f %n", obtenerAlturaMedia(personas));
+
+		System.out.println("La/s personas mas alta/s es/son: \n" + obtenerPersonaMasAlta(personas));
+
+		System.out.println("La/s personas mas baja/s es/son: \n" + obtenerPersonaMasBaja(personas));
+
+		System.out.println("La/s personas con una altura superior a la media son: " + listarSuperiorALaMedia(personas));
+
+		System.out.println("La/s personas con una altura superior a la media son: " + listarInferiorALaMedia(personas));
 
 		entrada.close();
 	}
 
 	public static void rellenarArray(String[][] personas) {
-		
+
 		int posicion;
 		String nombre, altura;
 		Scanner entrada = new Scanner(System.in);
 
-		for(posicion = 0; posicion < personas[1].length; posicion++){
+		for (posicion = 0; posicion < personas[1].length; posicion++) {
+			entrada = new Scanner(System.in);
 			System.out.println("Dame el nombre de la persona numero " + (posicion + 1));
-			nombre = entrada.next();
+			nombre = entrada.nextLine();
 			System.out.println("Dame la altura para " + nombre);
 			altura = Float.toString(entrada.nextFloat());
 
 			personas[0][posicion] = nombre;
 			personas[1][posicion] = altura;
+
 		}
+
 		entrada.close();
 	}
 
-	public static void mostrarPersonaMasAlta(String[][] personas) {
+	public static String obtenerPersonaMasAlta(String[][] personas) {
+		String resultado = "";
+		float alturaMaxima = obtenerAlturaMaxima(personas);
 
+		for (int i = 0; i < personas[1].length; i++) {
+			if (Float.parseFloat(personas[1][i]) == alturaMaxima) {
+				resultado += personas[0][i] + "\n";
+			}
+		}
+
+		return resultado;
 	}
 
-	public static void mostrarPersonaMasBaja(String[][] personas) {
+	public static String obtenerPersonaMasBaja(String[][] personas) {
+		String resultado = "";
+		float alturaMinima = obtenerAlturaMaxima(personas);
 
+		for (int i = 0; i < personas[1].length; i++) {
+			if (Float.parseFloat(personas[1][i]) == alturaMinima) {
+				resultado += personas[0][i] + "\n";
+			}
+		}
+
+		return resultado;
 	}
 
 	public static float obtenerAlturaMedia(String[][] personas) {
-		float media = 0;
 
-		return media;
-	}
-
-	public static void listarSuperiorALaMedia(String[][] personas) {
-
-	}
-
-	public static void listarInferiorALaMedia(String[][] personas) {
-
-	}
-
-	public static String mostrarArray(String[][] personas) {
-		String resultado = "";
+		float sumaAlturas = 0;
 
 		for (int i = 0; i < personas[1].length; i++) {
-			resultado = resultado + "Nombre: " + personas[0][i] + "\n" + "Altura: " + personas[1][i] + "\n";
+			sumaAlturas += Float.parseFloat(personas[1][i]);
 		}
+
+		return sumaAlturas / personas[1].length;
+	}
+
+	public static String listarSuperiorALaMedia(String[][] personas) {
+		String resultado = "";
+		float media = obtenerAlturaMedia(personas);
+
+		for (int i = 0; i < personas[1].length; i++) {
+			if (Float.parseFloat(personas[1][i]) > media) {
+				resultado += personas[0][i] + "\n";
+			}
+		}
+
 		return resultado;
+	}
+
+	public static String listarInferiorALaMedia(String[][] personas) {
+		String resultado = "";
+		float media = obtenerAlturaMedia(personas);
+
+		for (int i = 0; i < personas[1].length; i++) {
+			if (Float.parseFloat(personas[1][i]) < media) {
+				resultado += personas[0][i] + "\n";
+			}
+		}
+
+		return resultado;
+	}
+
+	public static float obtenerAlturaMaxima(String[][] personas) {
+		float maxima = 0;
+		for (int i = 0; i < personas[1].length; i++) {
+			maxima = (Float.parseFloat(personas[1][i]) > maxima) ? Float.parseFloat(personas[1][i]) : maxima;
+		}
+		return maxima;
+	}
+
+	public static float obtenerAlturaMinima(String[][] personas) {
+		float minima = 0;
+		for (int i = 0; i < personas[1].length; i++) {
+			minima = (Float.parseFloat(personas[1][i]) < minima) ? Float.parseFloat(personas[1][i]) : minima;
+		}
+		return minima;
+	}
+
+	public static void mostrarArray(String[][] personas) {
+
+		for (int i = 0; i < personas[1].length; i++) {
+			System.out.printf("Nombre: %s Altura: %.2f %n", personas[0][i], Float.parseFloat(personas[1][i]));
+		}
+
 	}
 
 }
